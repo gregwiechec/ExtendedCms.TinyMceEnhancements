d709a4956eaa72c05b8959301945d8b03632ece2
Grzegorz Wiechec <gwiechec@o2.pl>
2024-01-18 23:34:00 +0100

    Handle static attributes

--- src/ExtendedCms.TinyMceEnhancements/ClientResources/Scripts/advanced-image-plugin.js
+++ src/ExtendedCms.TinyMceEnhancements/ClientResources/Scripts/advanced-image-plugin.js
@@ -1,54 +1,57 @@
 window.tinymce.PluginManager.add("advanced-image-plugin", function (editor, url) {
 
-    function replaceImgUrlQuery(uri, key, value) {
-        var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-        var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-        if (uri.match(re)) {
-            return uri.replace(re, '$1' + key + "=" + value + '$2');
+    require(["extended-cms-tinymce-enhancements/settings"], function (settings) {
+        function replaceImgUrlQuery(uri, key, value) {
+            var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+            var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+            if (uri.match(re)) {
+                return uri.replace(re, '$1' + key + "=" + value + '$2');
+            }
+            else {
+                return uri + separator + key + "=" + value;
+            }
         }
-        else {
-            return uri + separator + key + "=" + value;
+
+        function updateImageUrl(imgEl) {
+            const width = imgEl.getAttribute("width");
+            const height = imgEl.getAttribute("height");
+
+            let src = imgEl.getAttribute("src");
+            src = replaceImgUrlQuery(src, "width", width);
+            src = replaceImgUrlQuery(src, "height", height);
+            ((settings.imageAttributes || {}).staticAttributes || []).forEach(function (attribute) {
+                src = replaceImgUrlQuery(src, attribute.name, attribute.value);
+            });
+            
+            imgEl.setAttribute("src", src);
+            imgEl.setAttribute("data-original", src);
+            imgEl.setAttribute("data-mce-src", src);
+
+
+            setTimeout(() => {
+                editor.fire("change");
+            }, 100);
         }
-    }
 
-    function updateImageUrl(imgEl) {
-        const width = imgEl.getAttribute("width");
-        const height = imgEl.getAttribute("height");
+        editor.on('ObjectResized', function (e) {
+            if (e.target.tagName === "IMG") {
+                const img = e.target;
+                updateImageUrl(img);
+            }
+        });
 
-        let src = imgEl.getAttribute("src");
-        src = replaceImgUrlQuery(src,"width", width);
-        src = replaceImgUrlQuery(src, "height", height);
-        src = replaceImgUrlQuery(src, "extendedconfiguration", true);
+        editor.on('ExecCommand', function (e) {
+            if (e.command !== "mceInsertContent") {
+                return;
+            }
+            setTimeout(() => {
+                const nodeEl = editor.selection.getNode();
+                const img = nodeEl.querySelector("img");
 
-        imgEl.setAttribute("src", src);
-        imgEl.setAttribute("data-original", src);
-        imgEl.setAttribute("data-mce-src", src);
-
-
-        setTimeout(() => {
-            editor.fire("change");
-        }, 100);
-    }
-    
-    editor.on('ObjectResized', function(e) {
-        if (e.target.tagName === "IMG") {
-            const img = e.target;
-            updateImageUrl(img);
-        }
+                updateImageUrl(img);
+            }, 100)
+        });
     });
-    
-    editor.on('ExecCommand', function(e) {
-        if (e.command !== "mceInsertContent") {
-            return;
-        }
-        setTimeout(() => {
-            const nodeEl = editor.selection.getNode();
-            const img = nodeEl.querySelector("img");
-
-            updateImageUrl(img);
-        }, 100)
-    });
-
     
     return {
         getMetadata: function () {
