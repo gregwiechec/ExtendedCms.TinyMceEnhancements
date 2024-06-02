01a4a04ce9879ca98de30ef3563fc1b560a0c077
Grzegorz Wiechec <gwiechec@o2.pl>
2024-06-02 22:10:00 +0200

    Add DisplayName for UserMacroVariable

--- src/ExtendedCms.TinyMceEnhancements/MacroVariables/MarcoVariables/UserMacroVariable.cs
+++ src/ExtendedCms.TinyMceEnhancements/MacroVariables/MarcoVariables/UserMacroVariable.cs
@@ -5,7 +5,9 @@ namespace ExtendedCms.TinyMceEnhancements.MacroVariables.MarcoVariables;
 public class UserMacroVariable(IPrincipalAccessor principalAccessor): ITinyMceMacroVariable
 {
     public string Key => "USER_NAME";
-    
+
+    public string DisplayName => "User name";
+
     public string GetValue()
     {
         return principalAccessor?.Principal?.Identity?.Name ?? "";
